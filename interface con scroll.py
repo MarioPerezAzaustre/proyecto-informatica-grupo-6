b1beb2356aa72700e93057cd62b77ba4cef528bf
MarioPerezAzaustre <mario.perez.azaustre@estudiantat.upc.edu>
2026-04-21 17:01:04 +0200

    V1

--- interface con scroll.py
+++ interface con scroll.py
@@ -10,7 +10,6 @@ color_anadir = "#FFF0B3"
 
 mis_aeropuertos = []
 
-
 def cargar_archivo():
     datos_temporales = LoadAirports("Airports.txt")
 
@@ -33,15 +32,24 @@ def ver_datos():
         messagebox.showwarning("Vacio", "La lista esta vacia. Carga el archivo primero.")
         return
 
-    texto_total = ""
+    ventana_lista = tk.Toplevel(root)
+    ventana_lista.title("Lista de Aeropuertos")
+    ventana_lista.geometry("400x300")
+    ventana_lista.configure(bg=color_fondo)
+
+    barra_scroll = tk.Scrollbar(ventana_lista, bg=color_fondo)
+    barra_scroll.pack(side=tk.RIGHT, fill=tk.Y)
+
+    lista_visual = tk.Listbox(ventana_lista, yscrollcommand=barra_scroll.set, font=("Courier", 10), bg="white", fg="black")
     for apt in mis_aeropuertos:
         if apt.schengen:
             estado = "Schengen"
         else:
             estado = "No Schengen"
-        texto_total += f"{apt.codigo}: {apt.latitud:.2f}, {apt.longitud:.2f} ({estado})\n"
+        lista_visual.insert(tk.END, f"{apt.codigo}: {apt.latitud:.2f}, {apt.longitud:.2f} ({estado})")
 
-    messagebox.showinfo("Lista de Aeropuertos", texto_total)
+    lista_visual.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10)
+    barra_scroll.config(command=lista_visual.yview)
 
 
 def guardar_txt():
@@ -82,7 +90,6 @@ def eliminar_existente():
     else:
         messagebox.showerror("Error", "No se encontro el aeropuerto en la lista.")
 
-
 root = tk.Tk()
 root.title("Panel de Control - Aeropuertos")
 root.geometry("650x450")
@@ -106,7 +113,7 @@ tk.Label(frame_inputs, text="Longitud:", bg=color_fondo, fg="black").grid(row=0,
 entrada_lon = tk.Entry(frame_inputs, width=12, bg="white", fg="black")
 entrada_lon.grid(row=0, column=5, padx=5, pady=10)
 
-tk.Button(frame_inputs, text="Anadir a la Lista", command=agregar_nuevo, bg=color_anadir, fg="black",  font=("Helvetica", 9, "bold")).grid(row=1, column=1, columnspan=2, padx=10, pady=10)
+tk.Button(frame_inputs, text="Anadir a la Lista", command=agregar_nuevo, bg=color_anadir, fg="black", font=("Helvetica", 9, "bold")).grid(row=1, column=1, columnspan=2, padx=10, pady=10)
 tk.Button(frame_inputs, text="Borrar por Codigo", command=eliminar_existente, bg=color_borrar, fg="black", font=("Helvetica", 9, "bold")).grid(row=1, column=3, columnspan=2, padx=10, pady=10)
 
 frame_datos = tk.LabelFrame(root, text=" 2. Gestion de Datos ", bg=color_fondo, fg="black")
@@ -124,3 +131,5 @@ tk.Button(frame_visual, text="Ver Grafica de Barras", command=mostrar_grafica, b
 tk.Button(frame_visual, text="Generar Mapa KML", command=mostrar_mapa, bg=color_visual, fg="black", width=22, font=("Helvetica", 10, "bold")).grid(row=1, column=0, padx=10, pady=8)
 
 root.mainloop()
+
+#Las funciones "Toplevel", "Listbox" y "Scrollbar" son de fuentes externas para crear la ventana emergente ya que si no los aeropuertos no cabian en la pantalla
